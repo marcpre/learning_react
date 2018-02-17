6eacd11c6b2425e6c2b9127b6cecf10627cd9705
marcpre <marcus.presich@gmail.com>
2018-02-17 16:40:18 +0100

    adding custom style to app

--- src/App.js
+++ src/App.js
@@ -13,14 +13,56 @@ class App extends Component {
       { name: 'adf', age: 26}
     ]
   }
+  
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'newName', age: 28},
+        { name: event.target.value, age: 9999999},
+        { name: 'lolonator', age: 26}
+    ]
+   })
+  }
     
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28},
+        { name: 'Maxddsdf', age: 9999999},
+        { name: 'lolonator', age: 26}
+    ]
+   })
+  }
   
   render() {
-    return ( <div className = "App" >
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+    
+    return ( 
+    <div className = "App" >
       <h1 > Hi, I am a react app. </h1>  
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
+        <button 
+        style={style}
+        onClick={this.switchNameHandler.bind(this, 'adsfsad')}> Switch Name </button>  
+        <Person 
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age} />
+        <Person 
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age} 
+          changed = {this.nameChangeHandler}/>
+        <Person 
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age} 
+          click={this.switchNameHandler} >
+            In the above paragraph the button switched gets also triggered.
+        </Person>
       </div>
     );
   }
