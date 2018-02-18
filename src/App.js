745cc1176fa1093c17b49fb496516672ab61cc39
marcpre <marcus.presich@gmail.com>
2018-02-18 10:34:45 +0100

    change name of list item

--- src/App.js
+++ src/App.js
@@ -8,21 +8,32 @@ class App extends Component {
   
   state = {
     persons: [
-      { name: 'Max', age: 28},
-      { name: 'Maxddsdf', age: 24},
-      { name: 'adf', age: 26}
+      { id: 'a12sdf', name: 'Max', age: 28},
+      { id: 'as23df',  name: 'Maxddsdf', age: 24},
+      { id: 'asd4f', name: 'adf', age: 26}
     ],
+    otherState: 'some other value',
     showPersons: false
   }
-  
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = (event, id) => {
+    
+    //get person of json
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    }); 
+    
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    
+    person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
     this.setState({
-      persons: [
-        { name: 'newName', age: 28},
-        { name: event.target.value, age: 9999999},
-        { name: 'lolonator', age: 26}
-    ]
+      persons: persons
    })
   }
   
@@ -31,15 +42,13 @@ class App extends Component {
     this.setState({showPersons: !doesShow})
   }
     
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28},
-        { name: 'Maxddsdf', age: 9999999},
-        { name: 'lolonator', age: 26}
-    ]
-   })
-  }
+deletePersonHandler = (personIndex) => {
+  // const persons = this.state.persons.slice();
+  //Creates a real copied object and does not change the main object
+  const persons = [...this.state.persons]; //spreads out the elements and adds to the new array
+  persons.splice(personIndex, 1);
+  this.setState({persons: persons});
+}
   
   render() {
     const style = {
@@ -55,33 +64,30 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-        <Person 
-          name = {this.state.persons[0].name} 
-          age = {this.state.persons[0].age} />
-        <Person 
-          name = {this.state.persons[1].name} 
-          age = {this.state.persons[1].age} 
-          changed = {this.nameChangeHandler}/>
-        <Person 
-          name = {this.state.persons[2].name} 
-          age = {this.state.persons[2].age} 
-          click={this.switchNameHandler} >
-            In the above paragraph the button switched gets also triggered.
-        </Person>
+          {/* mapping all list objects to the output */}
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click = {() => this.deletePersonHandler(index)}
+            name = {person.name} 
+            age = {person.age} 
+            key = {person.id} 
+            changed = {(event) => this.nameChangeHandler(event, person.id)}/>
+          })}
       </div> 
       );
     }
 
     return ( 
-    <div className = "App" >
-      <h1 > Hi, I am a react app. </h1>  
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}> Switch Name 
-        </button>
-        {persons}          
+      <div className = "App" >
+        <h1 > Hi, I am a react app. </h1>  
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}> Toggle Persons </button>
+          {persons}          
       </div>
     );
+  // JSX is rendered like the following below!  
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
